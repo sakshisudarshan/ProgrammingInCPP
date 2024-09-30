6103bfd8b3ee458a7a1f750af971a1d4c50947fa
Sakshi Sudarshan <144573279+sakshisudarshan@users.noreply.github.com>
2024-09-30 12:23:01 +0530

    Create Macros.cpp

---
+++ Macros.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define add(x,y) x+y
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int x;
+    // int y;
+    // cout<<"Please enter x and y values"<<endl;
+    // cin>>x;
+    // cin>>y;
+    // cout<<"The result is " <<add(x,y)<<endl;
+    cout<<"The result is " <<4*add(10,15)<<endl;
+    return 0;
+}
