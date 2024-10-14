f46baa382b2f6b24961052a85f0bcb567660ecb5
Sakshi Sudarshan <144573279+sakshisudarshan@users.noreply.github.com>
2024-10-14 12:07:43 +0530

    Create FriendFunction.cpp

---
+++ FriendFunction.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class EquilateralTriangle{
+    
+private:
+    float a;
+    float circumference;
+    float area;
+    
+public:
+    void setA(float length){
+        a=length;
+        circumference=a*3;
+        area=(1.73*a*a)/4;
+    }
+    
+    friend void PrintResults(EquilateralTriangle);
+};
+
+void PrintResults(EquilateralTriangle et){
+    cout<<"The circumference is="<<et.circumference<<endl;
+    cout<<"The area is="<< et.area<<endl;
+}
+
+int main(){
+    EquilateralTriangle et;
+    et.setA(3);
+    PrintResults(et);
+    
+}
