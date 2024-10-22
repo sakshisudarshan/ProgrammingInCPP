466c7651f6fbe1067fdd6575d432f00071adb00c
Sakshi Sudarshan <144573279+sakshisudarshan@users.noreply.github.com>
2024-10-22 10:31:26 +0530

    Update Static.cpp

--- Static.cpp
+++ Static.cpp
@@ -1,64 +1,25 @@
 #include <iostream>
 using namespace std;
 
-const int globalVal=100;
-
-class MyClass{
-public:
-    static const int staticVal=4;
-    int val;
-    mutable int mval;
+class StaticExample{
+    static int counter;
     
-    MyClass(int v=0):val(v),mval(0){}
-    
-    int getValue() const;
-    
-    void incr();
-    
-    static const int getStaticValue();
-    
-    void showGlobalValue() const;
-    
-    static void showConstParameter(const int& param);
+    public:
+        void incr(){
+            counter++;
+        }
+        
+        static void show(){
+            cout<<"Count="<<counter<<endl;
+        }
 };
 
-int MyClass::getValue() const{
-    return val;
-}
-
-const int MyClass::getStaticValue(){
-    return staticVal;
-}
-
-void MyClass::incr(){
-    val++;
-    mval++;
-}
-
-void MyClass::showGlobalValue() const{
-    cout<<"Global Value "<< ::globalVal<<endl;
-}
-
-void MyClass::showConstParameter(const int& param){
-    cout<<"Const Parameter Value "<<param<<endl;
-}
+int StaticExample::counter=1;
 
 int main(){
-    const MyClass obj(10);
     
-    cout<<"value "<<obj.getValue()<<endl;
-    obj.mval=1000;
-    cout<<"Modified mutable value "<<obj.mval<<endl;
-    
-    cout<<"static constant value "<<MyClass::getStaticValue()<<endl;
-    
-    MyClass notConstObj(11);
-    notConstObj.incr();
-    cout<<"incremented value "<<notConstObj.getValue()<<endl;
-    
-    obj.showGlobalValue();
-    
-    MyClass::showConstParameter(::globalVal);
-    
-    return 0;
+    StaticExample ob;
+    ob.incr();
+    ob.incr();
+    StaticExample::show();
 }
