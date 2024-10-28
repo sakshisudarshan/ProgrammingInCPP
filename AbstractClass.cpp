8f5b4ce2ce1a1cb6abe9dd8dbe8b865b503f5956
Sakshi Sudarshan <144573279+sakshisudarshan@users.noreply.github.com>
2024-10-28 12:06:29 +0530

    Create AbstractClass.cpp

---
+++ AbstractClass.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class DataBase{
+public:
+    virtual void getName(){
+        cout<<" function of database class"<<endl;
+    }
+};
+
+class Manager:public DataBase{
+public:
+    void getName(){
+        cout<<" function of Manager class"<<endl;
+    }
+};
+
+class Customer:public DataBase{
+public:
+    void getName(){
+        cout<<" function of Customer class"<<endl;
+    }
+};
+
+int main(){
+    DataBase *ptr;
+    Manager m;
+    ptr=&m;
+    ptr->getName();
+    
+    Customer c;
+    c.getName();
+    
+    return 0;
+}
