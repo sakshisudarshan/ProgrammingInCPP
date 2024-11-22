d68dc3122b28907fe1a1dc9abcdcbc469c1d1928
Sakshi Sudarshan <144573279+sakshisudarshan@users.noreply.github.com>
2024-11-22 10:42:49 +0530

    Update Vectors.cpp

--- Vectors.cpp
+++ Vectors.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(){
+    
     vector<int> v1;
     vector<int> v2 {10,20,30};
     vector<char> v3(6);
@@ -25,6 +26,32 @@ int main(){
         v6.push_back(10*(i+1));
     }
     cout<<"The current capacity of v6 is:"<<v6.capacity()<<endl;
+    cout<<"The current size of v6 is:"<<v6.size()<<endl;
+    v6.pop_back();
+    cout<<"The current capacity of v6 is:"<<v6.capacity()<<endl;
+    cout<<"The current size of v6 is:"<<v6.size()<<endl;
+    v6.pop_back();
+    v6.pop_back();
+    cout<<"The current capacity of v6 is:"<<v6.capacity()<<endl;
+    cout<<"The current size of v6 is:"<<v6.size()<<endl;
+    
+    for(int i=0;i<v6.size();i++){
+        cout<<v6[i]<<endl;
+    }
+    cout<<"First value in v6 is:"<<v6.front()<<endl;
+    cout<<"Last value in v6 is:"<<v6.back()<<endl;
+    cout<<"Value at index 3 in v6 is:"<<v6.at(3)<<endl;
+    
+    vector<int>:: iterator iter=v6.begin();
+    v6.insert(iter,00);
+    v6.insert(iter+3,35);
+    for(int i=0;i<v6.size();i++){
+        cout<<v6[i]<<endl;
+    }
+    
+    v6.clear();
+    cout<<"The current capacity of v6 is:"<<v6.capacity()<<endl;
+    cout<<"The current size of v6 is:"<<v6.size()<<endl;
     
     return 0;
 }
